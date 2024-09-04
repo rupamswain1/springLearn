61aa84530b6c6e1528d4666c9f2d0a3983615bd5
rupamswain1 <rupamswain1@gmail.com>
2024-09-04 10:41:39 +0530

    spring boot bootstrap

---
+++ SprinBootLearn/src/main/java/in/rupam/SprinBootLearn/SprinBootLearnApplication.java
@@ -0,0 +1,15 @@
+package in.rupam.SprinBootLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootLearnApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SprinBootLearnApplication.class, args);
+		System.out.println("Hello from Boot");
+	}
+
+}
---
+++ SprinBootLearn/src/test/java/in/rupam/SprinBootLearn/SprinBootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rupam.SprinBootLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
