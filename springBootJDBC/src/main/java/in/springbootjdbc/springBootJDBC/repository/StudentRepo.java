0edabefce2b936eeb82f2b3e88082e4a4fd73ade
rupamswain1 <rupamswain1@gmail.com>
2024-09-17 13:55:51 +0530

    setting rest project

--- springBootJDBC/src/main/java/in/springbootjdbc/springBootJDBC/repository/StudentRepo.java
+++ springBootJDBC/src/main/java/in/springbootjdbc/springBootJDBC/repository/StudentRepo.java
@@ -3,8 +3,11 @@ package in.springbootjdbc.springBootJDBC.repository;
 import in.springbootjdbc.springBootJDBC.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +31,24 @@ public class StudentRepo {
     }
 
     public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql = "select * from student";
+//        RowMapper<Student> mapper= new RowMapper<Student>() {
+//            @Override
+//            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                Student s = new Student();
+//                s.setRollNo(rs.getInt("rollno"));
+//                s.setName(rs.getString("name"));
+//                s.setMarks(rs.getInt("marks"));
+//                return s;
+//            }
+//        } ;
+
+        return jdbc.query(sql, (rs, rowNum) -> {
+            Student s = new Student();
+            s.setRollNo(rs.getInt("rollno"));
+            s.setName(rs.getString("name"));
+            s.setMarks(rs.getInt("marks"));
+            return s;
+        });
     }
 }
---
+++ springBootRestApi/src/main/java/in/rupam/springBootRestApi/HomeController.java
@@ -0,0 +1,13 @@
+package in.rupam.springBootRestApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello Worlds";
+    }
+}
