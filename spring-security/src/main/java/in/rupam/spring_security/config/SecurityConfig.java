45cd06d00d869955d24141ecb3e0e647c8e566fc
rupamswain1 <rupamswain1@gmail.com>
2024-10-13 17:42:49 +0530

    spring security

---
+++ spring-security/src/main/java/in/rupam/spring_security/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package in.rupam.spring_security.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
