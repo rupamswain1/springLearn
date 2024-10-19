8c06fd5a94e505d57a96db39695c3b8d45d7331c
rupamswain1 <rupamswain1@gmail.com>
2024-10-19 20:25:39 +0530

    Quiz App bootstrap

---
+++ QuizApp/src/test/java/in/rupam/QuizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rupam.QuizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
